35658d1be181d7aad991250fa2314a528e0c53bc
Igor Barros <igorbarrosrj@gmail.com>
2019-10-25 13:57:29 +0000

    Entidade pedido pt1

--- src/main/java/com/igorbarros/curso/entites/User.java
+++ src/main/java/com/igorbarros/curso/entites/User.java
@@ -1,13 +1,18 @@
 package com.igorbarros.curso.entites;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name ="tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -18,6 +23,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy ="Client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -61,6 +69,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -84,6 +96,10 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+
+
+
 	
 
 }
