15ee717706760389f674914f54da7a45eabfe1f8
Igor Barros <igorbarrosrj@gmail.com>
2019-11-04 10:46:08 +0000

    Subtotal

--- src/main/java/com/igorbarros/curso/entites/Order.java
+++ src/main/java/com/igorbarros/curso/entites/Order.java
@@ -101,7 +101,13 @@ public class Order implements Serializable {
 	   }
 	}
 	
-		
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrdemItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 
 	@Override
